38b31c64ab847f899ff6efe06ffca40cbef5389f
Anna Reid <41653027+AnnaReid22@users.noreply.github.com>
2021-01-03 16:11:11 -0800

    heroku update

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,7 +12,7 @@ class Contact extends React.Component {
                 <Header />
                 <Container style={{width:"100%", marginTop: "50px", marginBottom: "50px"}}>
                     <h1>Contact Me</h1>
-                    <p>Thank you so much for looking at my website!</p>
+                    <p>Thank you so much for looking at my website!!</p>
                 </Container>
                 <Container style={{width:"100%", marginTop: "50px", marginBottom: "100px"}}>
                     <div style={{marginBottom: "50px", textAlign:"center" }}>
